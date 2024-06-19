3836250fafb9f472bf2b4cc0bef3d2e2777ca99c
anirban00537 <anirban00537@gmail.com>
2024-06-19 12:12:40 -0700

    removed error

--- components/editor/sidebar/template.main.tsx
+++ components/editor/sidebar/template.main.tsx
@@ -2,41 +2,15 @@ import useCarousel from "@/hooks/useCarousel";
 import React from "react";
 
 const TemplateSection = () => {
-  const { selectedTheme, handleThemeChange } = useCarousel();
   return (
     <form className="grid w-full items-start gap-6 p-4">
       <legend className="-ml-1 px-1 text-2xl font-medium mb-4">Template</legend>
       <div className="grid gap-3">
-        <div
-          className="border p-3 text-center"
-          onClick={() => handleThemeChange("theme1")}
-        >
-          theme 1
-        </div>
-        <div
-          className="border p-3 text-center"
-          onClick={() => handleThemeChange("theme2")}
-        >
-          theme 2
-        </div>
-        <div
-          className="border p-3 text-center"
-          onClick={() => handleThemeChange("theme3")}
-        >
-          theme 3
-        </div>
-        <div
-          className="border p-3 text-center"
-          onClick={() => handleThemeChange("theme4")}
-        >
-          theme 4
-        </div>
-        <div
-          className="border p-3 text-center"
-          onClick={() => handleThemeChange("theme5")}
-        >
-          theme 5
-        </div>
+        <div className="border p-3 text-center">theme 1</div>
+        <div className="border p-3 text-center">theme 2</div>
+        <div className="border p-3 text-center">theme 3</div>
+        <div className="border p-3 text-center">theme 4</div>
+        <div className="border p-3 text-center">theme 5</div>
       </div>
     </form>
   );
