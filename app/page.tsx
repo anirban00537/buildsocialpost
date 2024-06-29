a8179dda322d517a90898459388f052bc233413e
anirban00537 <anirban00537@gmail.com>
2024-06-29 11:46:03 -0700

    updated design

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import FeaturesSection from "@/components/landing/features";
 import HowItWorksSection from "@/components/landing/howItWorks";
 import PlanSection from "@/components/landing/pricing";
 import Testimonial from "@/components/landing/testimonial";
+import Hero from "@/components/landing/hero";
 
 const Page = () => {
   const navigation = [
@@ -47,8 +47,8 @@ const Page = () => {
   ];
 
   return (
-    <div>
-      <>
+    <div className="mx-auto inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <div className="">
         <nav className="relative items-center pt-5 px-4 mx-auto max-w-screen-xl sm:px-8 md:flex md:space-x-6">
           <div className="flex justify-between items-center">
             <a href="javascript:void(0)">
@@ -61,14 +61,16 @@ const Page = () => {
             </a>
             <button
               className="text-gray-500 outline-none md:hidden"
-              onClick={() => setState(!state)}>
+              onClick={() => setState(!state)}
+            >
               {state ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke="currentColor">
+                  stroke="currentColor"
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -82,7 +84,8 @@ const Page = () => {
                   className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke="currentColor">
+                  stroke="currentColor"
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -98,7 +101,8 @@ const Page = () => {
               state
                 ? "absolute inset-x-0 px-4 border-b bg-white md:border-none md:static"
                 : "hidden"
-            }`}>
+            }`}
+          >
             <div className="items-center space-y-5 md:flex md:space-x-6 md:space-y-0 md:ml-12">
               {navigation.map((item, idx) => (
                 <li className="text-gray-500 hover:text-primary" key={idx}>
@@ -109,52 +113,16 @@ const Page = () => {
             <li className="order-2 py-5 md:py-0">
               <a
                 href="javascript:void(0)"
-                className="py-2 px-5 rounded-lg font-medium text-white text-center bg-primary hover:bg-primary/90  duration-150 block md:py-3 md:inline">
+                className="py-2 px-5 rounded-lg font-medium text-white text-center bg-primary hover:bg-primary/90  duration-150 block md:py-3 md:inline"
+              >
                 Get started
               </a>
             </li>
           </ul>
         </nav>
-        <motion.section
-          className="py-28"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}>
-          <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
-            <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
-              <h1 className="text-sm text-primary font-medium">
-                Create Stunning Carousels with AI
-              </h1>
-              <h2 className="text-4xl text-gray-800 md:leading-[58px] font-extrabold md:text-5xl">
-                Create Social media carousels in a few clicks.
-                <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-teal-400">
-                  with our AI tool .
-                </div>
-              </h2>
-              <p className="">
-                Our AI tool helps you generate content, customize designs, and
-                download your creations, simplifying your design process.
-              </p>
-              <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                <Link
-                  href="/editor"
-                  className="block py-2 px-4 text-center text-white font-medium bg-primary duration-150 hover:bg-primary/90 rounded-lg">
-                  Let's get started
-                </Link>
-              </div>
-            </div>
-            <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
-              <Image
-                src="/carousel.png"
-                className="md:rounded-tl-[108px]"
-                alt="Carousel Preview"
-                width={500}
-                height={500}
-              />
-            </div>
-          </div>
-        </motion.section>
-      </>
+
+        <Hero />
+      </div>
 
       <FeaturesSection />
 
@@ -162,7 +130,7 @@ const Page = () => {
 
       <PlanSection />
 
-    <Testimonial/>
+      <Testimonial />
 
       <footer className="pt-10 bg-white">
         <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
@@ -180,12 +148,14 @@ const Page = () => {
             <div className="items-center gap-x-3 space-y-3 sm:flex sm:justify-center sm:space-y-0">
               <Link
                 href="/editor"
-                className="block py-2 px-4 text-center text-white font-medium bg-primary duration-150 hover:bg-primary/90  rounded-lg hover:shadow-none">
+                className="block py-2 px-4 text-center text-white font-medium bg-primary duration-150 hover:bg-primary/90  rounded-lg hover:shadow-none"
+              >
                 Let's get started
               </Link>
               <a
                 href="#"
-                className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg"
+              >
                 Get access
                 <ChevronDown className="w-5 h-5" />
               </a>
@@ -197,7 +167,8 @@ const Page = () => {
               {footerNavs.map((item, idx) => (
                 <li
                   key={idx}
-                  className="text-gray-800 hover:text-gray-500 duration-150">
+                  className="text-gray-800 hover:text-gray-500 duration-150"
+                >
                   <a href={item.href}>{item.name}</a>
                 </li>
               ))}
---
+++ components/landing/hero.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import React from "react";
+import { motion } from "framer-motion";
+
+const textContainerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const textVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
+
+const Hero = () => {
+  return (
+    <motion.section
+      className="py-28 "
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.5 }}
+    >
+      <div className="max-w-screen-2xl mx-auto flex flex-col items-center text-center text-gray-600 gap-y-12 px-4 md:px-8">
+        <motion.div
+          className="space-y-5 sm:max-w-lg lg:max-w-5xl"
+          variants={textContainerVariants}
+        >
+          <motion.h1
+            className="text-base text-primary font-medium"
+            variants={textVariants}
+          >
+            Create Stunning Carousels with AI
+          </motion.h1>
+          <motion.h2
+            className="text-5xl text-gray-800 leading-[58px] font-extrabold md:text-7xl"
+            variants={textVariants}
+          >
+            Create Social media carousels in a few clicks.
+            <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-teal-400">
+              with our AI tool.
+            </div>
+          </motion.h2>
+          <motion.p className="text-lg" variants={textVariants}>
+            Our AI tool helps you generate content, customize designs, and
+            download your creations, simplifying your design process.
+          </motion.p>
+          <motion.div
+            className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0 justify-center"
+            variants={textVariants}
+          >
+            <Link
+              href="/editor"
+              className="block py-2 px-4 text-center text-white font-medium bg-primary duration-150 hover:bg-primary/90 rounded-lg"
+            >
+              Let's get started
+            </Link>
+          </motion.div>
+        </motion.div>
+        <div className="w-auto rounded-3xl flex justify-center bg-primary p-2">
+          <video
+            className="rounded-lg "
+            width="900"
+            height="600"
+            controls={false}
+            autoPlay={true}
+            muted
+            loop
+          >
+            <source src="/intro.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Hero;
