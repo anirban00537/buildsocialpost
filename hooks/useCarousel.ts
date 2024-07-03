ae5d2037c97678378dbf621e32e90a64ddcd9092
anirban00537 <anirban00537@gmail.com>
2024-07-03 09:30:31 +0600

    updated

--- hooks/useCarousel.ts
+++ hooks/useCarousel.ts
@@ -1,8 +1,6 @@
 import { useState, useCallback, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/state/store";
-import { jsPDF } from "jspdf";
-import { toPng } from "html-to-image";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import { Slide } from "@/types";
@@ -14,6 +12,7 @@ import {
   updateSlide,
   updateGeneralSettings,
 } from "@/state/slice/carousel.slice";
+import { toPng } from "html-to-image";
 
 const useCarousel = () => {
   const dispatch = useDispatch();
@@ -112,42 +111,28 @@ const useCarousel = () => {
 
   const exportSlidesToPDF = useCallback(async () => {
     setPdfLoading(true);
-    const pdf = new jsPDF("p", "px", [layout.width, layout.height]);
-    const scaleFactor = 3; // Adjust the scale factor for higher quality
+    try {
+      const slideIds = slides.map((_, index) => `slide-${index}`);
+      const response = await fetch("/api/generatePdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ slideIds, layout }),
+      });
 
-    for (let i = 0; i < slides.length; i++) {
-      const slideElement = document.getElementById(`slide-${i}`);
-      if (slideElement) {
-        try {
-          const pngDataUrl = await toPng(slideElement, {
-            cacheBust: true,
-            pixelRatio: scaleFactor,
-          });
-
-          if (i !== 0) {
-            pdf.addPage();
-          }
-          pdf.addImage(
-            pngDataUrl,
-            "PNG",
-            0,
-            0,
-            layout.width,
-            layout.height,
-            undefined,
-            "FAST" // Use FAST compression to reduce file size
-          );
-        } catch (error) {
-          console.error("Failed to export slide as image", error);
-        }
-      } else {
-        console.warn(`Slide element with ID slide-${i} not found`);
+      if (!response.ok) {
+        throw new Error("Failed to generate PDF");
       }
-    }
 
-    pdf.save("carousel_slides.pdf");
-    setPdfLoading(false);
-  }, [slides, layout.width, layout.height]);
+      const blob = await response.blob();
+      saveAs(blob, "carousel_slides.pdf");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setPdfLoading(false);
+    }
+  }, [slides, layout]);
 
   return {
     swiperRef,
