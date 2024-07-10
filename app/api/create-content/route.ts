995071d1a5d7a82246dd99a81179674f7858f613
anirban_itech <anirban00537@gmail.com>
2024-07-10 16:16:43 +0600

    fixed problem

--- app/api/create-content/route.ts
+++ app/api/create-content/route.ts
@@ -3,8 +3,6 @@ import {
   parseCarouselContentToJSON,
 } from "@/lib/openai";
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export async function POST(req: Request) {
   try {
     const { topic, numSlides, temperature, language, mood } = await req.json();
@@ -18,26 +16,18 @@ export async function POST(req: Request) {
       );
     }
 
-    const start = Date.now();
-
     const content: any = await generateCaruselContentFromTopic(
       topic,
       numSlides,
       temperature,
       language,
+      // model,
       mood
     );
-
+    // Generate carousel content
     console.log(content, "content");
 
     const response = parseCarouselContentToJSON(content);
-
-    const elapsed = Date.now() - start;
-    const remainingTime = 15000 - elapsed;
-
-    if (remainingTime > 0) {
-      await delay(remainingTime);
-    }
 
     return new Response(
       JSON.stringify({ message: "Carousel content generated", response }),
