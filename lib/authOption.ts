d6059e0557c1bce6033150ed898c196063c66594
anirban00537 <anirban00537@gmail.com>
2024-09-11 13:01:33 +0600

    added secret

--- lib/authOption.ts
+++ lib/authOption.ts
@@ -11,6 +11,7 @@ export const authOptions: AuthOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     session: async ({ session, user }) => {
       if (session?.user) {
