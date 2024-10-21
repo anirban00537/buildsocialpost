6eba7b6768edbb6d4a14dd5d1b0294485bf112bc
anirban00537 <anirban00537@gmail.com>
2024-10-21 14:39:04 +0600

    added random preset

--- components/layout/authCheckLayout.tsx
+++ components/layout/authCheckLayout.tsx
@@ -1,23 +1,36 @@
 "use client";
 import { useAuth } from "@/hooks/useAuth";
-import { usePathname } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
 import FullScreenLoading from "../loading/fullscreen.loading";
 import useBranding from "@/hooks/useBranding";
-import { setNewCarousel } from "@/state/slice/carousel.slice";
+import { setBackground, setNewCarousel } from "@/state/slice/carousel.slice";
 import { useDispatch } from "react-redux";
+import { darkColorPresets, lightColorPresets } from "@/lib/color-presets";
 
 const AuthCheckLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useAuth();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   useBranding();
   const dispatch = useDispatch();
+  const editorParam = searchParams.get("editor");
+
+  const chooseColorFromColorPalette = useCallback(() => {
+    const lightPreset =
+      lightColorPresets[Math.floor(Math.random() * lightColorPresets.length)];
+    const darkPreset =
+      darkColorPresets[Math.floor(Math.random() * darkColorPresets.length)];
+    const randomPreset = Math.random() < 0.5 ? lightPreset : darkPreset;
+    dispatch(setBackground(randomPreset));
+  }, [dispatch]);
 
   useEffect(() => {
     if (pathname) {
       dispatch(setNewCarousel());
+      if (!editorParam) chooseColorFromColorPalette();
     }
-  }, [pathname]);
+  }, [pathname, searchParams, dispatch, chooseColorFromColorPalette]);
 
   if (isLoading) {
     return <FullScreenLoading />;
