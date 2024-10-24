4d0b76a89f32ba362bd8f39a8be03ca789a0d199
anirban00537 <anirban00537@gmail.com>
2024-10-24 13:43:57 +0600

    no need to do seo this page

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -1,16 +1,5 @@
 import AuthCheckLayout from "@/components/layout/authCheckLayout";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Buildsocialpost - Generate Linkedin, TikTok, Instagram Carousel",
-  description:
-    "Create and share your own LinkedIn, TikTok, Instagram and Facebook carousel with ease",
-};
-
 export default function EditorLayout({
   children,
 }: {
